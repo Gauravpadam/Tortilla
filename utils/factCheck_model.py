6edb88175cde79abe752c42da7130fcc1d8abbc3
prmishra2 <PrinceMishra7>
2025-08-23 03:15:55 +0530

    ml pred for fake news

--- utils/factCheck_model.py
+++ utils/factCheck_model.py
@@ -1,24 +1,27 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-# Load the tokenizer and model
-model_name = "Pulk17/Fake-News-Detection"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+def ml_pred_fake_news(claim):
+    # Load the tokenizer and model
+    model_name = "Pulk17/Fake-News-Detection"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-# Your news/article text
-text = "Covid vaccination causes infertility in man."
+    
+    # Tokenize the input text
+    inputs = tokenizer(claim, return_tensors="pt", truncation=True, padding=True)
 
-# Tokenize the input text
-inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    # Get predictions
+    with torch.no_grad():
+        outputs = model(**inputs)
+        probs = torch.nn.functional.softmax(outputs.logits, dim=1)
+        label = torch.argmax(probs, dim=1).item()
+        prob = probs[0, label].item()
 
-# Get predictions
-with torch.no_grad():
-    outputs = model(**inputs)
-    probs = torch.nn.functional.softmax(outputs.logits, dim=1)
-    label = torch.argmax(probs, dim=1).item()
-    prob = probs[0, label].item()
+    # Map label to class
+    classes = ["FAKE", "REAL"]
+    print(f"Prediction: {classes[label]} (confidence: {prob:.2f})")
+    return {"verdict":classes[label],"confidence": prob}
 
-# Map label to class
-classes = ["FAKE", "REAL"]
-print(f"Prediction: {classes[label]} (confidence: {prob:.2f})")
+
+
