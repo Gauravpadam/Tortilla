61ebceebdf18b707804627149452e0e1287c7adc
PrinceMishra7 <iprincemisra@gmail.com>
2025-08-23 23:36:34 +0530

    tools

--- utils/ml_models.py
+++ utils/ml_models.py
@@ -1,11 +1,15 @@
+from typing import Annotated
+from portia import tool
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 from transformers import pipeline
 
-# -----------------------
-# Fake News Detector
-# -----------------------
-def ml_pred_fake_news(claim: str):
+@tool
+def ml_predict_fake_news(
+    claim: Annotated[str, "The news claim or headline to be analyzed for authenticity."]
+) -> dict:
+    """Uses a machine learning model to predict if a news claim is 'REAL' or 'FAKE' and provides a confidence score."""
+    
     model_name = "Pulk17/Fake-News-Detection"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
@@ -22,23 +26,36 @@ def ml_pred_fake_news(claim: str):
     return {"verdict": classes[label], "confidence": prob}
 
 
+@tool
+def ml_detect_fraud(
+    text: Annotated[str, "The text message or claim to be analyzed for fraudulent content."]
+):
+    """Uses a machine learning model to predict if a text is 'fraud' or 'normal' and provides a confidence score."""
 
-def fraud_detection(text:str):
     model_id = "austinb/fraud_text_detection"
     classifier = pipeline("text-classification", model=model_id)
-
-    # Example texts to classify
-    # scam_message = "Congratulations! You have won a free iPhone. Click this link to claim your prize."
-    # legitimate_message = "Your account has been successfully updated. No action is required from your side."
-
-    # Perform classification
+    
     scam_prediction = classifier(text)
-    return scam_prediction[0] #{'label': 'fraud', 'score': 0.977560818195343}
+    return scam_prediction[0]
+
+
+# def fraud_detection(text:str):
+#     model_id = "austinb/fraud_text_detection"
+#     classifier = pipeline("text-classification", model=model_id)
+
+#     # Example texts to classify
+#     # scam_message = "Congratulations! You have won a free iPhone. Click this link to claim your prize." {'label': 'fraud', 'score': 0.977560818195343}
+#     # legitimate_message = "Your account has been successfully updated. No action is required from your side." {'label': 'normal', 'score': 0.9629361629486084}
+
+#     # Perform classification
+#     scam_prediction = classifier(text)
+#     return scam_prediction[0] #{'label': 'fraud', 'score': 0.977560818195343}
 
 
 # -----------------------
 # Example usage
 # -----------------------
 if __name__ == "__main__":
-    print(ml_pred_fake_news("Covid-19 is caused by 5G"))
-    print(fraud_detection("Congratulations! You won a free ticket to Bahamas. Reply YES to claim your prize."))
+    pass
+    # print(ml_pred_fake_news("Covid-19 is caused by 5G"))
+    # print(fraud_detection("Your account has been successfully updated. No action is required from your side."))
